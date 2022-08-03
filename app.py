888fb163086ee5fcdf38f2bab62584d8f70abc30
lusha17 <antonlushkin2307@yandex.ru>
2022-08-03 10:14:57 +0300

    init

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import sys
+import gc
+from detect_clothes import func_detect_clothes
+from streamlit.legacy_caching import clear_cache
+
+def main():
+    gc.enable()
+    st.header("Computer Vision Demo")
+
+    st.sidebar.markdown("""<center data-parsed=""><img src="http://drive.google.com/uc?export=view&id=1Mad62XWdziqcx9wijUODpzGzqYEGhafC" align="center"></center>""",unsafe_allow_html=True,)
+    st.sidebar.markdown(" ")
+    
+    def reload():
+        clear_cache()
+        gc.collect()
+        st.experimental_rerun()
+
+    pages = st.sidebar.columns([1, 1, 1])
+    
+    pages[0].markdown(" ")
+
+    if pages[1].button("Reload App"):
+        reload()
+    func_detect_clothes()
+    
+main()
