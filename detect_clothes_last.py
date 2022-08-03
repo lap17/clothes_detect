e4316546cec8f5a2a478d3cedd414f58d9f54584
lusha17 <antonlushkin2307@yandex.ru>
2022-08-03 14:23:26 +0300

    add logs

--- detect_clothes_last.py
+++ detect_clothes_last.py
@@ -26,7 +26,7 @@ def func_detect_clothes():
             rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
             media_stream_constraints={"video": True, "audio": False},)
 
-    DEFAULT_CONFIDENCE_THRESHOLD = 0.4
+    DEFAULT_CONFIDENCE_THRESHOLD = 0.3
 
     result_queue = [] 
 
@@ -68,22 +68,16 @@ def func_detect_clothes():
         for bbox_data in result:
             xmin, ymin, xmax, ymax, conf, label = bbox_data
             if conf > confidence_threshold:
-                xmin = int(xmin)
-                ymin = int(ymin)
-                xmax = int(xmax)
-                ymax = int(ymax)
-                p0, p1, label = (xmin, ymin), (xmax, ymax), int(label)
+                p0, p1, label = (int(xmin), int(ymin)), (int(xmax), int(ymax)), int(label)
                 img = cv2.rectangle(img, p0, p1, rgb_colors[label], 2) 
                 ytext = ymin - 10 if ymin - 10 > 10 else ymin + 15
                 xtext = xmin + 10
                 class_ = CLASSES[label]
                 text_for_vis = '{} {}'.format(class_, str(conf.round(2)))
                 img = cv2.putText(img, text_for_vis, (int(xtext), int(ytext)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, rgb_colors[label], 2,)
-                if agree:
+                if (class_ == 'firearm') | (class_ == 'knife'):
                     time_detect = datetime.datetime.now(pytz.timezone("America/New_York")).replace(tzinfo=None).strftime("%m-%d-%y %H:%M:%S")
-                    cropped_image = img[ymin:ymax, xmin:xmax]
-                    #retval, buffer_img= cv2.imencode('.jpg', img[ymin:(ymax-ymin), xmin:(xmax-xmin)])
-                    retval, buffer_img= cv2.imencode('.jpg', cropped_image)
+                    retval, buffer_img= cv2.imencode('.jpg', img)
                     data = base64.b64encode(buffer_img).decode("utf-8")
                     html = "<img src='data:image/jpg;base64," + data + f"""' style='display:block;margin-left:auto;margin-right:auto;width:200px;border:0;'>"""
                     with lock:
@@ -129,11 +123,7 @@ def func_detect_clothes():
             for bbox_data in result_copy:
                 xmin, ymin, xmax, ymax, conf, label = bbox_data
                 if conf > confidence_threshold:
-                    xmin = int(xmin)
-                    ymin = int(ymin)
-                    xmax = int(xmax)
-                    ymax = int(ymax)
-                    p0, p1, label = (xmin, ymin), (xmax, ymax), int(label)
+                    p0, p1, label = (int(xmin), int(ymin)), (int(xmax), int(ymax)), int(label)
                     img_draw = cv2.rectangle(img_draw, p0, p1, rgb_colors[label], 2) 
                     ytext = ymin - 10 if ymin - 10 > 10 else ymin + 15
                     xtext = xmin + 10
@@ -146,13 +136,3 @@ def func_detect_clothes():
         ctx = webrtc_streamer(key="example", video_frame_callback=transform,
             rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
             media_stream_constraints={"video": True, "audio": False}, mode=WebRtcMode.SENDRECV, async_processing=True)
-        agree = st.checkbox("Enable clothes logging", value=False)
-        if agree:
-            if ctx.state.playing:
-                labels_placeholder = st.empty()
-                while True:
-                    time.sleep(0.5)
-                    with lock:
-                        result_queue = result_queue[:5]
-                        df = pd.DataFrame(result_queue)
-                        labels_placeholder.write(df.to_html(escape=False), unsafe_allow_html=True)
